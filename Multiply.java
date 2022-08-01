2f9f927de9212b7c173ce50612d8dca0389a056e
technolab <kenpatel2010@gmail.com>
2022-08-01 11:34:40 +0530

    Added Multiply.java file

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+ public class Multiply {
+ 
+  Run | Debug
+  public static void main(String[] args) {
+  init a = 4;
+  init b = 6;
+  
+  System.out.println("Ths product is" + (a+b)
+  }
+  
+  }
